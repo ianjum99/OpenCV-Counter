6e74db57c3597be3abcb681eaedcccb59bbfb40b
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2021-10-16 17:50:32 +0100

    Update people_counter.py

--- people_counter.py
+++ people_counter.py
@@ -5,7 +5,7 @@
 #	--output output/output_01.avi
 #
 # To read from webcam and write back out to disk:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+# python3 people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
 #	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
 #	--output output/webcam_output.avi
 
